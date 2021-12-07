47b0eac9f1bf757ade6f95cfde1154ab2ae1f8c7
Noah Sherwin <noahrsherwin@gmail.com>
2021-12-06 16:24:01 -0800

    build(cmake): add CMake files

---
+++ MSBuildTools.ps1
@@ -0,0 +1,21 @@
+if ($null -eq (Get-Module VSSetup)) {
+  Install-Module VSSetup
+}
+Import-Module VSSetup
+
+try {
+  Import-Module ".\ext\BinPosh.VSSetup\Get-VSInstallDir\BinPosh.GVSID.psd1"
+  Import-Module ".\ext\BinPosh.VSSetup\Enter-VSDevEnv\BinPosh.EVSDE.psd1"
+}
+catch {
+  git.exe submodule init
+  git.exe submodule sync
+  Import-Module ".\ext\BinPosh.VSSetup\Get-VSInstallDir\BinPosh.GVSID.psd1"
+  Import-Module ".\ext\BinPosh.VSSetup\Enter-VSDevEnv\BinPosh.EVSDE.psd1"
+}
+
+$Filter = (Get-Content .\MSBuildDeps.txt).Split("`n")
+$VSInstallDir = Get-VSInstallDir -Prerelease -Filter $Filter
+Enter-VSDevEnv -VSInstallDir $VSInstallDir
+
+cmd.exe (Get-ChildItem $VSInstallDir -Filter 'vcvarsall.bat' -Recurse -File).FullName
